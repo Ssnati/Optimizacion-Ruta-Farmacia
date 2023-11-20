234d82008d7599bc05e2111a0524d64ea2003457
JuanArchila13 <juanpabloarchila2003@outlook.com>
2023-11-19 23:24:00 -0500

    Conexión paquetes

--- connection/conection.py
+++ connection/conection.py
@@ -1,4 +1,4 @@
-from model import *
+from src.model import *
 import sqlite3 as sq
 import networkx as nx
 import matplotlib.pyplot as plt
---
+++ manage.py
@@ -0,0 +1,6 @@
+from src.model import *
+from connection.conection import *
+from windows.window_main import *
+
+app = MainWindow()
+app.mainloop()
